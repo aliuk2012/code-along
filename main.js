c146b327223fecbd8380c62ca33e3e799fa8e149
Ben Foxall <ben@pusher.com>
2016-07-31 15:41:29 +0100

    clean up and extract code

---
+++ main.js
@@ -0,0 +1,47 @@
+var htm = document.getElementsByTagName('html')[0]
+toggle.onclick = function(e){
+  e.preventDefault()
+  htm.classList.toggle('fill')
+}
+
+var code = CodeMirror(document.getElementById('code'), {
+  value: "document.body.style.background = '#08f'",
+  mode:  "javascript",
+  lineWrapping: true,
+  // readOnly: true
+})
+
+var textarea = document.querySelector('textarea')
+var iframe   = document.querySelector('iframe')
+var error_el = document.getElementById('error')
+
+var template = fetch('template.txt')
+                 .then(function(r){return r.text()})
+
+function load(){
+  convert(code.getValue())
+    .then(function(code){
+      template
+        .then(function(text) {
+
+          URL.revokeObjectURL(iframe.src)
+
+          var blob = new Blob([text
+            .split('{{INJECT}}')
+            .join(code)], {type: 'text/html'})
+
+          var url = URL.createObjectURL(blob)
+          iframe.src = url
+        })
+    })
+  .then(function(ok){
+    error_el.style.display = 'none'
+  }, function(err){
+    error_el.style.display = 'block'
+    error_el.innerText = err
+  })
+
+}
+
+load()
+code.on('change', load)
