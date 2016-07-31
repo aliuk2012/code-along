5a28c23d1ff4bd43e195d2335a3318c9a0f341cc
Ben Foxall <ben@pusher.com>
2016-07-31 14:29:22 +0100

    pull out conversion code

---
+++ convert/convert.js
@@ -0,0 +1,42 @@
+// building is problematic because acorn.es
+// import buble from './buble.deps.js'
+// import rollup from './rollup.browser.js'
+
+var modules = [
+
+  'draw', 'greeter'
+
+].reduce(function(memo, item){
+  memo[item] =
+    fetch('es_modules/' + item + '.js')
+      .then(function(r){ return r.text()})
+  return memo
+}, {})
+
+function convert (source) {
+
+  return rollup.rollup({
+    entry: '__main__',
+    plugins:[
+      {
+        resolveId: function(importer, importee) {
+          return importer
+        },
+        load: function(id) {
+          if(id == '__main__') return source
+          return modules[id] || Promise.reject("")
+        }
+      },
+      {
+        transformBundle: function(d){
+          return buble.transform(d).code
+        }
+      }
+    ]
+  })
+  .then(function(bundle){
+    return bundle.generate({
+      format: 'iife'
+    }).code
+  })
+}
---
+++ es_modules/draw.js
@@ -0,0 +1,18 @@
+
+const bodyElementStyle = document.body.style
+
+export function circle(){
+  console.log("TODO: draw circle")
+}
+
+export function square(){
+  console.log("TODO: draw square")
+}
+
+export function arrow(){
+  console.log("TODO: draw arrow")
+}
+
+export function background(color){
+  bodyElementStyle.background = color
+}
