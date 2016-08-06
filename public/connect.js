e2764d86b20470d74ad19fd861708050b73ccf50
Ben Foxall <ben@pusher.com>
2016-08-06 09:31:38 +0100

    more store hooking up

--- public/connect.js
+++ public/connect.js
@@ -1,76 +1,49 @@
-var source = []
-
-function store(user, key, value) {
-  user  = norm(user, 10)
-  key   = norm(key, 10)
-  value = norm(value, 50)
-
-  if(!(
-    user && key && value
-  )) return false
-
-  var user_count = 0
-  source = source.filter(function(s){
-    // TODO - check if array.splice would be more efficient
-    return !(
-      s[0] == user && (
-        s[1] == key || // key matches
-        user_count++ > 10 // we've see too many by this user
-      )
-    )
-  })
-
-  source.unshift([user, key, value])
-
-  while(source.length > 2000) {
-    source.pop()
-  }
+var store = {getAll: function() {return []}, set: function(){}}
 
 
-  function norm(t, l){
-    return String(t).trim().substr(0,l||10)
-  }
-}
-
-function set(key, data) {
-  fetch('/store',{
-    method: 'POST',
-    body: JSON.stringify({
-      key: key,
-      value: data
-    }),
-    credentials:'same-origin',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).then(function(d){console.log(d)})
-}
-
-function getAll(key){
-  return source.filter(function(item){
-    return item[1] == key
-  }).map(function(item){
-    return item[2]
-  })
-}
-
-
+// initial populate
 var populate = fetch('/content')
   .then(function(res){ return res.text()})
   .then(function(text){
-    console.log("content:" + text)
     code.setValue(text)
   })
 
+var pusherClient = fetch('/pusher/config')
+  .then(function(res){return res.json()})
+  .then(function(config){return new Pusher(config.key, config.options)})
 
-// don't listen if we're the master (other checks too)
-if(document.location.hash != '#master') {
+/*
+  "login" to the channel & set up store
+*/
+pusherClient.then(function(pusher){
 
-  fetch('/pusher/config')
-    .then(function(res){return res.json()})
-    .then(function(config){
-      var pusher = new Pusher(config.key, config.options)
+  var presence = pusher.subscribe('presence-codealong')
+  presence.bind('pusher:subscription_succeeded', function(e){
+
+    var element = document.getElementById('connection-state')
+    element.style.display = 'block'
+    element.className = 'connected'
+    element.innerText = '_'+e.myID
+
+    store = remoteStore(pusher, e.myID)
+  })
+
+})
+
+
+
+// there are backend checks too
+var isMaster = document.location.hash != '#master'
+
+
+/*
+  Listen for code updates
+*/
+if(!isMaster) {
+  // not if we're pushing out as the master
+
+  pusherClient
+    .then(function(pusher){
 
       var channel = pusher.subscribe('codealong')
 
@@ -84,49 +57,15 @@ if(document.location.hash != '#master') {
         }
 
       })
-
-      // Pusher.logToConsole = true
-
-      var presence = pusher.subscribe('presence-codealong')
-      presence.bind('pusher:subscription_succeeded', function(e){
-        var element = document.getElementById('connection-state')
-        element.style.display = 'block'
-        element.className = 'connected'
-        element.innerText = '_'+e.myID
-      })
-
-
-      fetch('/store')
-        .then(function(res) {return res.json()})
-        .then(function(json) {
-          source = json
-        })
-      var storeC = pusher.subscribe('codealong_store')
-      storeC.bind('add', function(event){
-        (event.rows||[]).forEach(function(row){
-          store.apply(null, row)
-        })
-      })
-
-      /*
-      fetch('/store',{
-          method: 'POST',
-          body: JSON.stringify({
-            key: 'THE KEY',
-            value: 'MY VALUE'
-          }),
-          credentials:'same-origin',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          }
-        }).then(function(d){console.log(d)})
-      */
-
-
     })
+}
 
-} else {
+
+/*
+  Send code updates
+*/
+
+if(isMaster) {
 
   console.log("Master!")
 
@@ -160,4 +99,107 @@ if(document.location.hash != '#master') {
 
     })
 
+}
+
+function remoteStore(pusher){
+
+  var state = []
+
+  // populate
+  fetch('/store')
+    .then(function(res) {return res.json()})
+    .then(function(json) {
+      state = json
+    })
+
+  // connect
+  pusher.subscribe('codealong_store')
+    .bind('add', function(event){
+      (event.rows||[]).forEach(function(row){
+        store.apply(null, row)
+      })
+    })
+
+
+  function store(user, key, value) {
+    user  = norm(user, 10)
+    key   = norm(key, 10)
+    value = norm(value, 50)
+
+    if(!(
+      user && key && value
+    )) return false
+
+    var user_count = 0
+    state = state.filter(function(s){
+      // TODO - check if array.splice would be more efficient
+      return !(
+        s[0] == user && (
+          s[1] == key || // key matches
+          user_count++ > 10 // we've see too many by this user
+        )
+      )
+    })
+
+    state.unshift([user, key, value])
+
+    while(state.length > 2000) {
+      state.pop()
+    }
+
+
+    function norm(t, l){
+      return String(t).trim().substr(0,l||10)
+    }
+  }
+
+  function set(key, data) {
+    fetch('/store',{
+      method: 'POST',
+      body: JSON.stringify({
+        key: key,
+        value: data
+      }),
+      credentials:'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  function getAll(key){
+    return state.filter(function(item){
+      return item[1] == key
+    }).map(function(item){
+      return item[2]
+    })
+  }
+
+  var timers = {}
+  function setDebounced(key, data) {
+    clearTimeout(timers[key])
+    timers[key] = setTimeout(function(){
+      fetch('/store',{
+        method: 'POST',
+        body: JSON.stringify({
+          key: key,
+          value: data
+        }),
+        credentials:'same-origin',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+    }, 500)
+  }
+
+  return {
+    set: setDebounced,
+    getAll: getAll,
+    source: function(){
+      return source
+    }
+  }
 }
--- public/es_modules/store.js
+++ public/es_modules/store.js
@@ -1,3 +1,12 @@
 export function get(key) {
   return Promise.resolve('foo')
 }
+
+export function set(key, value) {
+  return window.parent.store.set(key, value)
+}
+
+
+export function getAll(key) {
+  return window.parent.store.getAll(key)
+}
